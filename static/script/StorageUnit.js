1a979912cf243c4486824cb6a25436dbe371ad98
Javad <Javad.Sf.Talebi@outlook.com>
2024-01-23 11:59:33 +0330

    add storage unit

---
+++ static/script/StorageUnit.js
@@ -0,0 +1,28 @@
+const STATIC_PROGRAM = "static_Program", INTERVAL_PRPGRAM = "interval_Program";
+
+let isStorageExist = () => {
+    let intervalProgram = localStorage.getItem(INTERVAL_PRPGRAM);
+    let staticProgram = localStorage.getItem(STATIC_PROGRAM);
+    return (staticProgram != null && intervalProgram != null) ? true : false;
+}
+
+let createCalendarStorage = () => {
+    localStorage.setItem(INTERVAL_PRPGRAM, JSON.stringify({}));
+    localStorage.setItem(STATIC_PROGRAM, JSON.stringify([]));
+}
+
+let getIntervalProgram = () => {
+    return localStorage.getItem(INTERVAL_PRPGRAM);
+}
+
+let setIntervalProgram = (interval_Program = {}) => {
+    localStorage.setItem(INTERVAL_PRPGRAM, JSON.stringify(interval_Program));
+}
+
+let getStaticProgram = () => {
+    return localStorage.getItem(STATIC_PROGRAM);
+}
+
+let setStaticProgram = (static_Program = []) => {
+    localStorage.setItem(STATIC_PROGRAM, JSON.stringify(static_Program));
+}
