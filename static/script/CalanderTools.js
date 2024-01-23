e65af26953c74d8ac515566c1203846519788d22
Javad <Javad.Sf.Talebi@outlook.com>
2024-01-24 00:29:10 +0330

    import calendar tools code

---
+++ static/script/CalanderTools.js
@@ -0,0 +1,62 @@
+let putEventHourAndState = (obj) => {
+    Object.keys(obj).forEach(key => {
+        document.querySelector(`#td_${key}`).textContent = obj[key];
+    });
+}
+
+let changeRadioButtonvisibility = (radioButtonName, type) => {
+    let radio = document.querySelectorAll(`input[name="${radioButtonName}"]`);
+    for (let i = 0; i < radio.length; i++) {
+        radio[i].disabled = type;
+    }
+}
+
+let putDescriptionInEvent = (tagElem, value) => {
+    let elem = document.querySelector(tagElem);
+    elem.value = value;
+}
+
+let putProgramStateRadioButton = (programState, type) => {
+    let radios = document.querySelectorAll('input[name="scheduleTypeCalendar"]')
+    for (const iterator of radios) {
+        if (iterator.value == programState) { iterator.checked = true; }
+    }
+}
+
+let addNewEventObj = (obj) => {
+    let o = {
+        id: obj.id,
+        sTime: getCustomeTimeObject(obj.start),
+        eTime: getCustomeTimeObject(obj.end),
+        start: new Date(obj.start).getTime(),
+        end: new Date(obj.end).getTime(),
+        title: obj.title,
+        color: obj.color,
+        state: getKeyByValue(PROGRAM_STATE, "color", obj.color).state
+    }
+    calendar.addEvent(obj);
+    calendar.unselect();
+    serializedEventsArry.push(o);
+}
+
+let editEventById = (id, arry, state) => {
+    for (let i = 0; i < arry.length; i++) {
+        if (id == arry[i].id) {
+            arry[i].title = DESCRIPTION_ELEMENT.value;
+            arry[i].programState = state;
+            arry[i].color = getKeyByValue(PROGRAM_STATE, "state", state).color;
+        }
+    }
+}
+
+let updateCalendarEvents = () => {
+    let intervalProgram = diserializedIntervalArry.filter(obj => {
+        return obj.programState == PROGRAM_STATE["interval"].state;
+    });
+    let staticProgarm = diserializedIntervalArry.filter(obj => {
+        return obj.programState == PROGRAM_STATE["specefic"].state;
+    })
+
+    setIntervalProgram(intervalProgram);
+    setStaticProgram(staticProgarm);
+}
---
+++ static/script/Tools.js
@@ -0,0 +1,16 @@
+let getKeyByValue = (object, searchKey, value) => {
+    let o = Object.keys(object).find(key =>
+        object[key][searchKey] === value);
+    return object[o];
+}
+
+let createCustomeTime = (time) => {
+    let date = new Date(time);
+    let minute = (String(date.getMinutes()).length == 2) ? date.getMinutes() : `0${date.getMinutes()}`;
+    return `${date.getHours()}:${minute}`;
+}
+
+let getCustomeTimeObject = (time) => {
+    let date = new Date(time);
+    return [date.getHours(), date.getMinutes()];
+}
